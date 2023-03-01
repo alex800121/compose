28fe9b5bfee5e96ac1bb7d50939f0422ff6962c7
alex800121 <alex800121@hotmail.com>
2023-03-01 10:22:36 +0800

    First working compose

--- src/Compose.hs
+++ src/Compose.hs
@@ -1,33 +1,25 @@
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE DataKinds #-}
--- {-# LANGUAGE UndecidableInstances #-}
--- {-# LANGUAGE NoImplicitPrelude #-}
 
 module Compose where
-import Data.Kind (Type)
 
 -- compose :: (a -> b) -> a -> b (i ~ a, o ~ b)
 -- compose :: (a -> b) -> (c -> a) -> c -> b (i ~ (c -> a), o ~ (c -> b))
 -- compose :: (a -> b) -> (c -> d -> a) -> c -> d -> b (i ~ (c -> d -> a), o ~ (c -> d -> b))
 
-data Nat = Z | S Nat
+type family HasArgs a b i where
+  HasArgs a b a = b
+  HasArgs a b (c -> d) = c -> HasArgs a b d
 
-type family CountArgs a where
-  CountArgs (c -> a) = S (CountArgs a)
-  CountArgs a = Z
+class Compose a b i where
+  compose :: (a -> b) -> i -> HasArgs a b i
 
-type family R f i o where
-  R (a -> b) a b = a -> b
-  R f (c -> i) (c -> o) = R f i o
-
-class (R f i o ~ f) => Reduce f i o where
-  compose :: f -> i -> o
-instance (b ~ o) => Reduce (a -> b) a o where
+instance {-# INCOHERENT #-} (HasArgs a b i ~ b, a ~ i) => Compose a b i where
   compose = id
-instance (R (a -> b) i o ~ R (a -> b) (c -> i) (d -> o), Reduce (a -> b) i o, c ~ d) => Reduce (a -> b) (c -> i) (d -> o) where
-  compose f i x = compose f (i x)
-  
 
-head' :: String -> Char
-head' = head
+instance {-# OVERLAPS #-} (HasArgs a b (c -> d) ~ (c -> HasArgs a b d), Compose a b d) => Compose a b (c -> d) where
+  compose f x y = compose f (x y)
+
+(.:) :: Compose a b i => (a -> b) -> i -> HasArgs a b i
+(.:) = compose
+
+infixr 9 .:
